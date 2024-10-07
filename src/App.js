6bafbc872cfbc6ad64f085d0587a1314c7220aa9
xShir0s <mtodys2006@gmail.com>
2024-10-07 12:56:39 +0200

    Dodanie Plikow Dagrasso Strona

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div className="Banner">
+            <img src="dagrassoLogo.webp" className="Dagrasso"/>
+
+
+        </div>
+
+        <div className="main">
+
+            <div className="imageContainer">
+                <img src="horizontaline.png" className="Hz"/>
+            </div>
+            <div className="Pizza1">
+                <img src="13_wiejska.webp" className="Wiejska"/>
+                <p className="Normal">Pizza Wiejksa, Bardzo Dobra
+                    <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
+                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                </p>
+
+            </div>
+            <div className="Pizza2">
+                <img src="5_decoro.webp" className="Wiejska"/>
+                <p className="Normal">Pizza Decoro, Bardzo Dobra
+                    <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
+                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                </p>
+            </div>
+            <div className="Pizza3">
+                <img src="17_da-grasso.webp" className="Wiejska"/>
+                <p className="Normal">Pizza Dagrasso, Bardzo Dobra
+                    <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
+                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                </p>
+            </div>
+            <div className="Pizza4">
+                <img src="4_campione.webp" className="Wiejska"/>
+                <p className="Normal">Pizza Campione, Bardzo Dobra
+                    <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
+                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                </p>
+            </div>
+            <div className="Pizza5">
+                <img src="2_capriciosa.webp" className="Wiejska"/>
+                <p className="Normal">Pizza Capriciosa, Bardzo Dobra
+                    <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
+                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                </p>
+            </div>
+
+
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
