7a2a3049e857363a3bd5fd7e7b1ee740ea1d55da
xShir0s <mtodys2006@gmail.com>
2024-10-07 13:57:25 +0200

    Poprawienie Tekstow

--- src/App.js
+++ src/App.js
@@ -17,38 +17,38 @@ function App() {
             </div>
             <div className="Pizza1">
                 <img src="13_wiejska.webp" className="Wiejska"/>
-                <p className="Normal">Pizza Wiejksa, Bardzo Dobra
+                <p className="Normal">Pizza Wiejksa
                     <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
-                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                    <p className="Hint2">Cena: 40zl, 70zl, 120zl</p>
                 </p>
 
             </div>
             <div className="Pizza2">
                 <img src="5_decoro.webp" className="Wiejska"/>
-                <p className="Normal">Pizza Decoro, Bardzo Dobra
+                <p className="Normal">Pizza Decoro
                     <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
-                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                    <p className="Hint2">Cena: 40zl, 70zl, 120zl</p>
                 </p>
             </div>
             <div className="Pizza3">
                 <img src="17_da-grasso.webp" className="Wiejska"/>
-                <p className="Normal">Pizza Dagrasso, Bardzo Dobra
+                <p className="Normal">Pizza Dagrasso
                     <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
-                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                    <p className="Hint2">Cena: 40zl, 70zl, 120zl</p>
                 </p>
             </div>
             <div className="Pizza4">
                 <img src="4_campione.webp" className="Wiejska"/>
-                <p className="Normal">Pizza Campione, Bardzo Dobra
+                <p className="Normal">Pizza Campione
                     <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
-                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                    <p className="Hint2">Cena: 40zl, 70zl, 120zl</p>
                 </p>
             </div>
             <div className="Pizza5">
                 <img src="2_capriciosa.webp" className="Wiejska"/>
-                <p className="Normal">Pizza Capriciosa, Bardzo Dobra
+                <p className="Normal">Pizza Capriciosa
                     <p className="Hint">Rozmiar: 32cm, 64cm, 80cm</p>
-                    <p className="Hint">Cena: 40zl, 70zl, 120zl</p>
+                    <p className="Hint2">Cena: 40zl, 70zl, 120zl</p>
                 </p>
             </div>
 
